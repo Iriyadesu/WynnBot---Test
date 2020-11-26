a74524605849eb0744c7b3ea0aa312d9baf504c8
Illya <egyeske04@freemail.hu>
2020-11-26 22:29:33 +0100

    Creating categories

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 # ---------- commands ----------
 
 
@@ -60,86 +61,92 @@ async def on_member_join(member):
 
 
 # ----- moderation commands -----
+class Moderation(commands.Cog):
+    """Moderation commands"""
+    # ----- ban
+    @bot.command(help="Bans someone.") 
+    @commands.has_permissions(ban_members=True)
+    async def ban(ctx, user: discord.Member, *, reason="No reason provided"):
+            await user.ban(reason=reason)
+            ban = discord.Embed(title=f":boom: Banned {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+            await ctx.message.delete()
+            await ctx.channel.send(embed=ban)
+            await user.send(embed=ban)
 
-# ----- ban
-@bot.command(help="Bans someone.") # Need to add reason as optional arg
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, user: discord.Member, *, reason="No reason provided"):
-
-        await user.ban(reason=reason)
-        ban = discord.Embed(title=f":boom: Banned {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
-        await ctx.message.delete()
-        await ctx.channel.send(embed=ban)
-        await user.send(embed=ban)
-
-# ----- kick
-@bot.command(help="Kicks someone.") # Need to add reason as optional arg
-@commands.has_permissions(ban_members=True)
-async def kick(ctx, user: discord.Member, *, reason="No reason provided"):
-        await user.kick(reason=reason)
-        ban = discord.Embed(title=f":boom: Kicked {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
-        await ctx.message.delete()
-        await ctx.channel.send(embed=ban)
-        await user.send(embed=ban)
-
+    # ----- kick
+    @bot.command(help="Kicks someone.")  
+    @commands.has_permissions(ban_members=True)
+    async def kick(ctx, user: discord.Member, *, reason="No reason provided"):
+            await user.kick(reason=reason)
+            ban = discord.Embed(title=f":boom: Kicked {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+            await ctx.message.delete()
+            await ctx.channel.send(embed=ban)
+            await user.send(embed=ban)
+bot.remove_command("ban")
+bot.remove_command("kick")
+bot.add_cog(Moderation())
 
 # ----- wynn info commands -----
+class Wynncraft(commands.Cog):
+    """Wynncraft related commands"""
+    # ----- player stats
+    @bot.command(help="Let's you view a player's profile.") # Need to make it look better
+    async def profile(ctx, PlayerName):
 
-# ----- player stats
-@bot.command(help="Let's you view a player's profile.") # Need to make it look better
-async def profile(ctx, PlayerName):
+        r = req.get(f'https://api.wynncraft.com/v2/player/{PlayerName}/stats')
+        #get the data
+        json = r.json()
+        #Print the data
+        if json['code'] == 400:
+            return await ctx.channel.send('Cannot find user.') 
+        highestlvl = 0
+        for x in json['data'][0]['classes']:
+            if x['professions']['combat']['level'] > highestlvl:
+                highestlvl = x['professions']['combat']['level']
+        embedVar = discord.Embed(title=f"{PlayerName}'s profile", color=0x00ff00)
+        embedVar.add_field(name="UserName: ", value=f"{json['data'][0]['username']}", inline=True)
+        embedVar.add_field(name="Rank: ", value=f"{json['data'][0]['meta']['tag']['value']}", inline=False)
+        embedVar.add_field(name="Online: ", value=f"{json['data'][0]['meta']['location']['online']}", inline=True)
+        embedVar.add_field(name="Server: ", value=f"{json['data'][0]['meta']['location']['server']}", inline=True)
+        embedVar.add_field(name = chr(173), value = chr(173))
+        embedVar.add_field(name="Guild's name: ", value=f"{json['data'][0]['guild']['name']}", inline=True)
+        embedVar.add_field(name="Rank in guild: ", value=f"{json['data'][0]['guild']['rank']}", inline=True)
+        embedVar.add_field(name="Playtime: ", value=f"{int((json['data'][0]['meta']['playtime'] *4.7)/60)} hours.", inline=False)
+        embedVar.add_field(name="Highest Level: ", value=f"{highestlvl}", inline=False)
+        embedVar.add_field(name="Joined: ", value=f"{json['data'][0]['meta']['firstJoin'][:10]}", inline=True)
+        await ctx.channel.send(embed=embedVar)
+        # await ctx.send(json['data'][0]['rank'])
 
-    r = req.get(f'https://api.wynncraft.com/v2/player/{PlayerName}/stats')
-    #get the data
-    json = r.json()
-    #Print the data
-    if json['code'] == 400:
-        return await ctx.channel.send('Cannot find user.') 
-    highestlvl = 0
-    for x in json['data'][0]['classes']:
-        if x['professions']['combat']['level'] > highestlvl:
-            highestlvl = x['professions']['combat']['level']
-    embedVar = discord.Embed(title=f"{PlayerName}'s profile", color=0x00ff00)
-    embedVar.add_field(name="UserName: ", value=f"{json['data'][0]['username']}", inline=True)
-    embedVar.add_field(name="Rank: ", value=f"{json['data'][0]['meta']['tag']['value']}", inline=False)
-    embedVar.add_field(name="Online: ", value=f"{json['data'][0]['meta']['location']['online']}", inline=True)
-    embedVar.add_field(name="Server: ", value=f"{json['data'][0]['meta']['location']['server']}", inline=True)
-    embedVar.add_field(name = chr(173), value = chr(173))
-    embedVar.add_field(name="Guild's name: ", value=f"{json['data'][0]['guild']['name']}", inline=True)
-    embedVar.add_field(name="Rank in guild: ", value=f"{json['data'][0]['guild']['rank']}", inline=True)
-    embedVar.add_field(name="Playtime: ", value=f"{int((json['data'][0]['meta']['playtime'] *4.7)/60)} hours.", inline=False)
-    embedVar.add_field(name="Highest Level: ", value=f"{highestlvl}", inline=False)
-    embedVar.add_field(name="Joined: ", value=f"{json['data'][0]['meta']['firstJoin'][:10]}", inline=True)
-    await ctx.channel.send(embed=embedVar)
-    # await ctx.send(json['data'][0]['rank'])
+    # ----- guild stats
+    @bot.command(help="Let's you view a guild's info.") #? Need to make it look better
+    async def guild(ctx, GuildName):
 
-# ----- guild stats
-@bot.command(help="Let's you view a guild's info.") #? Need to make it look better
-async def guild(ctx, GuildName):
+        r = req.get(f'https://api.wynncraft.com/public_api.php?action=guildStats&command={GuildName}')
 
-    r = req.get(f'https://api.wynncraft.com/public_api.php?action=guildStats&command={GuildName}')
+        json = r.json()
 
-    json = r.json()
+        if "error" in json:
+            return await ctx.channel.send('Cannot find guild.') 
 
-    if "error" in json:
-        return await ctx.channel.send('Cannot find guild.') 
+        embedVar = discord.Embed(title=f"{GuildName}", color=0xFF0000)
+        embedVar.add_field(name="Name: ", value=f"{json['name']}", inline=False)
+        embedVar.add_field(name="Prefix: ", value=f"{json['prefix']}", inline=False)
+        embedVar.add_field(name="Level: ", value=f"{json['level']}", inline=False)
+        embedVar.add_field(name="Members: ", value=f"{len(json['members'])}", inline=False)
+        embedVar.add_field(name="Territories: ", value=f"{json['territories']}", inline=False)
+        # embedVar.add_field(name="Playtime: ", value=f"{json['data'][0]['meta']['playtime'] /60} hours.", inline=False)
+        embedVar.add_field(name="Created at: ", value=f"{json['createdFriendly']}", inline=False)
+        await ctx.channel.send(embed=embedVar)
 
-    embedVar = discord.Embed(title=f"{GuildName}", color=0xFF0000)
-    embedVar.add_field(name="Name: ", value=f"{json['name']}", inline=False)
-    embedVar.add_field(name="Prefix: ", value=f"{json['prefix']}", inline=False)
-    embedVar.add_field(name="Level: ", value=f"{json['level']}", inline=False)
-    embedVar.add_field(name="Members: ", value=f"{len(json['members'])}", inline=False)
-    embedVar.add_field(name="Territories: ", value=f"{json['territories']}", inline=False)
-    # embedVar.add_field(name="Playtime: ", value=f"{json['data'][0]['meta']['playtime'] /60} hours.", inline=False)
-    embedVar.add_field(name="Created at: ", value=f"{json['createdFriendly']}", inline=False)
-    await ctx.channel.send(embed=embedVar)
-
-# ----- item stats
-@bot.command(help="Shows you the stats of an item.") #! Need to finish this
-async def item(ctx, ItemName):
-    r = req.get(f'https://api.wynncraft.com/public_api.php?action=itemDB&search={ItemName}')
-    json = r.json()
-
+    # ----- item stats
+    @bot.command(help="Shows you the stats of an item.") #! Need to finish this
+    async def item(ctx, ItemName):
+        r = req.get(f'https://api.wynncraft.com/public_api.php?action=itemDB&search={ItemName}')
+        json = r.json()
+bot.remove_command("profile")
+bot.remove_command("guild")
+bot.remove_command("item")
+bot.add_cog(Wynncraft())
 
 # ---------- run the bot ----------
 try:
