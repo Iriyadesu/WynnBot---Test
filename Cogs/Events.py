c99d5ea24db38c322ce52d6e80d3f9b3df46176c
Illya <egyeske04@freemail.hu>
2020-11-26 23:22:39 +0100

    Fix...again

---
+++ Cogs/Events.py
@@ -0,0 +1,36 @@
+from discord.ext import commands
+import discord
+
+class Events(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    #Handling errors
+    @commands.Cog.listener()
+    async def on_command_error(self,ctx, error):
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Please pass in all requirements :rolling_eyes:.')
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You dont have all the requirements :angry:")
+
+    # Making commands case insensitive
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        temp = message.content.split(" ")
+        message.content = str(temp[0].lower())
+        for x in temp[1:]:
+            message.content += " " + str(x)
+
+    # Giving out Guest role when user joins
+    @commands.Cog.listener()
+    async def on_member_join(self,member):
+        try:
+            await member.add_roles(discord.utils.get(member.guild.roles, name='guest')) 
+        except Exception as e:
+            await print('Cannot assign role. Error: ' + str(e))
+        await bot.process_commands(message)
+
+        
+def setup(bot):
+    bot.add_cog(Events(bot))
--- main.py
+++ main.py
@@ -27,45 +27,13 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
-# ---------- commands ----------
-
-
-# Handling command errors
-@bot.event
-async def on_command_error(ctx, error):
-
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Please pass in all requirements :rolling_eyes:.')
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You dont have all the requirements :angry:")
-
-# Making commands case insensitive
-@bot.event
-async def on_message(message):
-    temp = message.content.split(" ")
-    message.content = str(temp[0].lower())
-    for x in temp[1:]:
-        message.content += " " + str(x)
-
-    await bot.process_commands(message)
-
-
-# Giving out Guest role when user joins
-@bot.event
-async def on_member_join(member):
-    try:
-        await member.add_roles(discord.utils.get(member.guild.roles, name='guest')) 
-    except Exception as e:
-        await print('Cannot assign role. Error: ' + str(e))
-
-
 # ---------- registering commands ----------
-extensions = ['Cogs.Moderation', 'Cogs.Wynncraft']
+extensions = ['Cogs.Moderation', 'Cogs.Wynncraft', 'Cogs.Events']
 
 if __name__ == '__main__':
     for ext in extensions:
         bot.load_extension(ext)
+
 
 # ---------- run the bot ----------
 try:
@@ -76,5 +44,3 @@ finally:
     os.rename('bot.log', name)
     shutil.move(name, './logs')
 
-def setup(bot):
-    bot.add_cog(Moderation(bot))
