c268aa8b9d03af6567f0c3e9326f8fd49b0a2a8f
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2020-11-27 14:20:34 +0100

    removed indent in Moderation.py

--- Cogs/Moderation.py
+++ Cogs/Moderation.py
@@ -7,11 +7,11 @@ class Moderation(commands.Cog):
      #  ----- ban -----
     @commands.command(description="Bans someone")
     async def ban(self,ctx, user: discord.Member, *, reason="No reason provided"):
-            await user.ban(reason=reason)
-            ban = discord.Embed(title=f":boom: Banned {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
-            await ctx.message.delete()
-            await ctx.channel.send(embed=ban)
-            await user.send(embed=ban)
+        await user.ban(reason=reason)
+        ban = discord.Embed(title=f":boom: Banned {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+        await ctx.message.delete()
+        await ctx.channel.send(embed=ban)
+        await user.send(embed=ban)
     # ----- kick -----
     @commands.command(description="kicks someone")
     async def kick(self,ctx, user: discord.Member, *, reason="No reason provided"):
