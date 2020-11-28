ca9f34d1915029ead8240a6e26b69f605f2c28ae
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2020-11-28 14:29:58 +0100

    More formatting changes

--- Cogs/Moderation.py
+++ Cogs/Moderation.py
@@ -3,15 +3,17 @@ import discord
 
 
 class Moderation(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
 
     #  ----- ban -----
     @commands.command(description="Bans someone")
     @commands.has_permissions(ban_members=True)
-    async def ban(self,ctx, user: discord.Member, *, reason="No reason provided"):
+    async def ban(self, ctx, user: discord.Member, *, reason="No reason provided"):
         await user.ban(reason=reason)
-        ban = discord.Embed(title=f":boom: Banned {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+        ban = discord.Embed(title=f":boom: Banned {user.name}!",
+                            description=f"Reason: {reason}\nBy: {ctx.author.mention}"
+                            )
         await ctx.message.delete()
         await ctx.channel.send(embed=ban)
         await user.send(embed=ban)
@@ -19,9 +21,11 @@ class Moderation(commands.Cog):
     # ----- kick -----
     @commands.command(description="kicks someone")
     @commands.has_permissions(kick_members=True)
-    async def kick(self,ctx, user: discord.Member, *, reason="No reason provided"):
+    async def kick(self, ctx, user: discord.Member, *, reason="No reason provided"):
         await user.kick(reason=reason)
-        ban = discord.Embed(title=f":boom: Kicked {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+        ban = discord.Embed(title=f":boom: Kicked {user.name}!",
+                            description=f"Reason: {reason}\nBy: {ctx.author.mention}"
+                            )
         await ctx.message.delete()
         await ctx.channel.send(embed=ban)
         await user.send(embed=ban)
@@ -29,7 +33,7 @@ class Moderation(commands.Cog):
     # ----- repeat -----
     @commands.command(description="speak beep boop")
     @commands.has_permissions(administrator=True)
-    async def say(self,ctx,*,arg="Nothing"):
+    async def say(self, ctx, *, arg="Nothing"):
         try: 
             await ctx.channel.send(str(arg))
         except Exception as e:
