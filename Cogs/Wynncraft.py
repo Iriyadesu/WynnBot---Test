8ffe4e4148260c0ec40c8a630f88e49c0e8fbd00
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2020-11-29 19:24:48 +0100

    Added !territory command

--- Cogs/Wynncraft.py
+++ Cogs/Wynncraft.py
@@ -3,6 +3,7 @@ import discord
 import requests as req
 
 from Wrappers.player import Player
+from Wrappers.territory import Territory
 
 
 class Wynncraft(commands.Cog):
@@ -16,27 +17,27 @@ class Wynncraft(commands.Cog):
 
         if stat is not None:
             try:
-                embedVar = discord.Embed(title=f"{player_name}'s profile", color=0x00ff00)
-                embedVar.add_field(name=stat, value=player[stat])
-                await ctx.channel.send(embed=embedVar)
+                player_embed = discord.Embed(title=f"{player_name}'s profile", color=0x00ff00)
+                player_embed.add_field(name=stat, value=player[stat])
+                await ctx.channel.send(embed=player_embed)
                 return
             except:
                 await ctx.channel.send('requested stat was not found')
                 return
 
-        embedVar = discord.Embed(title=f"{player_name}'s profile", color=0x00ff00)
-        embedVar.add_field(name="UserName: ", value=f"{player['username']}", inline=True)
-        embedVar.add_field(name=chr(173), value=chr(173))
-        embedVar.add_field(name="Rank: ", value=f"{player['rank']}", inline=False)
-        embedVar.add_field(name="Online: ", value=f"{player['location']}", inline=True)
-        embedVar.add_field(name=chr(173), value=chr(173))
-        embedVar.add_field(name="Guild name: ", value=f"{player['guild name']}", inline=True)
-        embedVar.add_field(name="Guild rank: ", value=f"{player['guild rank'].lower() if player['guild rank'] else 'none'}",
+        player_embed = discord.Embed(title=f"{player_name}'s profile", color=0x00ff00)
+        player_embed.add_field(name="UserName: ", value=f"{player['username']}", inline=True)
+        player_embed.add_field(name=chr(173), value=chr(173))
+        player_embed.add_field(name="Rank: ", value=f"{player['rank']}", inline=False)
+        player_embed.add_field(name="Online: ", value=f"{player['location']}", inline=True)
+        player_embed.add_field(name=chr(173), value=chr(173))
+        player_embed.add_field(name="Guild name: ", value=f"{player['guild name']}", inline=True)
+        player_embed.add_field(name="Guild rank: ", value=f"{player['guild rank'].lower() if player['guild rank'] else 'none'}",
                            inline=True)
-        embedVar.add_field(name="Playtime: ", value=f"{player['total playtime']} hours.", inline=False)
-        embedVar.add_field(name="Highest Level: ", value=f"{player['highest level combat']}", inline=False)
-        embedVar.add_field(name="Joined: ", value=f"{player['first join'][:10]}", inline=True)
-        await ctx.channel.send(embed=embedVar)
+        player_embed.add_field(name="Playtime: ", value=f"{player['total playtime']} hours.", inline=False)
+        player_embed.add_field(name="Highest Level: ", value=f"{player['highest level combat']}", inline=False)
+        player_embed.add_field(name="Joined: ", value=f"{player['first join'][:10]}", inline=True)
+        await ctx.channel.send(embed=player_embed)
     
     #  ----- guild stats -----
     @commands.command(description="Let's you search for guilds.") #? Need to make it look better
@@ -48,15 +49,32 @@ class Wynncraft(commands.Cog):
         if "error" in data:
             return await ctx.channel.send('Cannot find guild.') 
 
-        embedVar = discord.Embed(title=f"{guild_name}", color=0xFF0000)
-        embedVar.add_field(name="Name: ", value=f"{data['name']}", inline=False)
-        embedVar.add_field(name="Prefix: ", value=f"{data['prefix']}", inline=False)
-        embedVar.add_field(name="Level: ", value=f"{data['level']}", inline=False)
-        embedVar.add_field(name="Members: ", value=f"{len(data['members'])}", inline=False)
-        embedVar.add_field(name="Territories: ", value=f"{data['territories']}", inline=False)
+        guild_embed = discord.Embed(title=f"{guild_name}", color=0xFF0000)
+        guild_embed.add_field(name="Name: ", value=f"{data['name']}", inline=False)
+        guild_embed.add_field(name="Prefix: ", value=f"{data['prefix']}", inline=False)
+        guild_embed.add_field(name="Level: ", value=f"{data['level']}", inline=False)
+        guild_embed.add_field(name="Members: ", value=f"{len(data['members'])}", inline=False)
+        guild_embed.add_field(name="Territories: ", value=f"{data['territories']}", inline=False)
         # embedVar.add_field(name="Playtime: ", value=f"{json['data'][0]['meta']['playtime'] /60} hours.", inline=False)
-        embedVar.add_field(name="Created at: ", value=f"{data['createdFriendly']}", inline=False)
-        await ctx.channel.send(embed=embedVar)
+        guild_embed.add_field(name="Created at: ", value=f"{data['createdFriendly']}", inline=False)
+        await ctx.channel.send(embed=guild_embed)
+
+    #  ----- territory stats -----
+    @commands.command(description='Sends info about requested guild\nUse `\"` for more-word names.')
+    async def territory(self, ctx, territory_name):
+        terr = Territory(territory_name)
+        if terr._data is None:
+            await ctx.channel.send(f'Territory \"{territory_name}\" was not found')
+            return
+
+        territory_embed = discord.Embed(title=f"{territory_name}", color=0xFF0000)
+        territory_embed.add_field(name='Name:', value=territory_name)
+        territory_embed.add_field(name='Owner:', value=terr['owner'])
+        territory_embed.add_field(name=chr(173), value=chr(173))
+        territory_embed.add_field(name='Start coords:', value=f'{terr["location"]["startX"]} {terr["location"]["startZ"]}')
+        territory_embed.add_field(name='End coords:', value=f'{terr["location"]["endX"]} {terr["location"]["endZ"]}')
+
+        await ctx.channel.send(embed=territory_embed)
 
     #  ----- item stats -----
     @commands.command(description="Search for items") #! Need to finish this
---
+++ Wrappers/territory.py
@@ -0,0 +1,69 @@
+from requests import get
+
+
+class Territory:
+    """
+    Class containing all information for Wynncraft territory (1.19)
+    """
+    def __init__(self, name: str):
+        """
+        1) gets data
+        2) checks for errors (codes 400, 429, anything except 200)
+        3) decodes json and assigns data to 'data' variable
+        5) creates dictionary containing all data
+        """
+        # sends request
+        res = get('https://api.wynncraft.com/public_api.php?action=territoryList')
+
+        if res.status_code == 400:
+            # if status code is 400 (non-existing name)
+            raise NameError(f'name was not found')
+
+        elif res.status_code == 429:
+            # if too many requests are sent (exceeded the limit)(750/30min/ip)
+            raise Exception('Too many requests!')
+
+        elif res.status_code != 200:
+            # other errors
+            raise Exception(f'Cannot procede. Status code: {res.status_code}')
+
+        else:
+            # status code is 200
+
+            # gets the json
+            jres = res.json()
+
+        data = jres['territories']
+        if name not in data:
+            self._data = None
+            return
+        terr = data[name]
+
+        self._data = {
+            'name': name,
+            'owner': terr['guild'],
+            'acquired': terr['acquired'],
+            'location': {
+                'startX': terr['location']['startX'],
+                'startZ': terr['location']['startY'],
+                'endX': terr['location']['endX'],
+                'endZ': terr['location']['endY']
+            }
+        }
+
+    def __getitem__(self, key):
+        """
+        Allows to access data by doing object[key].
+        try:
+            return self.data[key]
+        except KeyError:
+            raise KeyError('Player %s doesn't have \'%s\' stat' %(self['username'], key))
+        """
+
+        try:
+            return self._data[key]
+        except KeyError:
+            raise KeyError(f'Property \"{key}\" was not found in {self["name"]}')
+
+    def __repr__(self):
+        return f'<territory {self["name"]}>'
