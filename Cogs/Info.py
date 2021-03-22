7ce2a86ab48a050626b0a1bddad7a798e91c9d19
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2021-03-22 12:25:54 +0100

    Info.py: Added DnD dice roll commad ("r")

--- Cogs/Info.py
+++ Cogs/Info.py
@@ -1,4 +1,6 @@
 import asyncio as aio
+import re
+import random as rnd
 
 import bot_data as bd
 
@@ -208,6 +210,44 @@ class Info(commands.Cog):
             await ctx.send(str(arg))
         except Exception as e:
             await ctx.send('Cannot assign role. Error: ' + str(e))
+
+    @commands.command()
+    async def r(self, ctx: commands.Context, *args):
+        """
+        Command for rolling dice for DnD
+
+        :param ctx: channel where the command was used
+        :param args: roll message (in format {number of rolls}d{max roll} {optional: +/-number)
+        :return:
+        """
+        # 1st group is number of rolls
+        # 2nd group is the range <1; number> (inclusive)
+        # 3rd group is addition number to add/subtract
+        match = re.search(r'(\d+)d(\d+) *([+-] *\d+)?', ''.join(args))  # regex for getting the rolls
+        if match is not None:  # if found
+            roll_list = []
+            for index in range(int(match.group(1))):
+                roll_list.append(rnd.randint(1, int(match.group(2))))
+
+            if match.group(3) is not None:  # TODO: fix "!r 1d20 +- 8" not raising an error
+                last_part = f' {match.group(3)} = {sum(roll_list) + int(match.group(3))}'  # last part of the embed (the sum)
+            else:
+                last_part = f' +0 = {sum(roll_list)}'
+
+            embed = discord.Embed(
+                title=f'{match.group(1)}d{match.group(1)} rolls',
+                color=bd.embed_colors['normal'],
+                description=f'SUM: {sum(roll_list)}' + last_part
+            )
+            for index in range(len(roll_list)):  # display all rolls
+                embed.add_field(name=f'Roll #{index+1}', value=roll_list[index])
+
+            await ctx.send(embed=embed)
+
+        else:
+            await ctx.send(embed=bd.error_embed(
+                'Human error', 'Invalid roll format'  # TODO: Make it better
+            ))
 
 
 def setup(bot: commands.Bot):
