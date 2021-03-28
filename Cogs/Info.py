5d22e5b653591d723c74eb31d96eb3ba89ccb9fa
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2021-03-28 23:13:05 +0200

    Moved some commands to Miscellaneous.py

--- Cogs/Info.py
+++ Cogs/Info.py
@@ -1,5 +1,3 @@
-import asyncio as aio
-
 import bot_data as bd
 
 from discord.ext import commands
@@ -13,80 +11,6 @@ class Info(commands.Cog):
 
     def __init__(self, bot: discord.ext.commands.Bot):
         self.bot = bot
-
-    @commands.command(pass_context=True, description="creates/ends poll",
-                      usage="!poll <create|end> <name|message id> [options]")
-    async def poll(self, ctx: commands.Context, cord: str, var, *options: str):  # TODO: add documentation, a lot of it
-        """
-        Manages a poll.
-        - create
-          - !poll create <question> <options separated by space>
-        - end
-          - !poll end <poll id>
-
-        FriendlyPudding shall document it themselves
-
-        :param ctx: channel where the command was used
-        :param cord: whether to create or end poll
-        :param var: The (Ultimate) question (of Life, the Universe, and Everything)
-        :param options: vote options
-        :return: None
-        """
-        if cord.lower() == "create":  # creating a poll
-            if len(options) < 2:  # not enough options
-                await ctx.send('You need more than one option to make a poll!')
-                return
-            if len(options) > 10:  # too many options
-                await ctx.send('You cannot make a poll for more than 10 things!')
-                return
-            if len(options) == 2 and options[0].lower() == 'yes' and options[1].lower() == 'no':
-                reactions = ['✅', '❌']  # yes/no poll get special reactions
-            else:  # proper poll
-                reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']  # TODO: Maybe use unicode code?
-                # reactions = [
-                #     '\U000020E3', '\U000020E3', '\U000020E3', '\U000020E3', '\U000020E3',
-                #     '\U000020E3', '\U000020E3', '\U000020E3', '\U000020E3', '\U0001F51F'
-                # ]  # This one apparently doesn't work (I wonder why...)
-
-            description = []  # create embed text
-            for x, option in enumerate(options):
-                description += '\n {} {}'.format(reactions[x], option)
-
-            embed = discord.Embed(title=var, description=''.join(description), color=0x0000FF)
-            react_message = await ctx.send(embed=embed)
-
-            for reaction in reactions[:len(options)]:
-                await react_message.add_reaction(reaction)
-
-            embed.set_footer(text='Poll ID: {}'.format(react_message.id))
-
-            await react_message.edit(embed=embed)
-
-        elif cord.lower() == "end":  # ending a poll
-            # TODO: FriendlyPudding please clear this mess, or at least comment it
-            poll_message = await ctx.channel.fetch_message(var)
-            embed = poll_message.embeds[0]
-            unformatted_options = [x.strip() for x in embed.description.split('\n')]
-            opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
-                else {x[:1]: x[2:] for x in unformatted_options}
-
-            # check if we're using numbers for the poll, or x/checkmark, parse accordingly
-            voters = [self.bot.user.id]  # add the bot's ID to the list of voters to exclude it's votes
-            tally = {x: 0 for x in opt_dict.keys()}
-            for reaction in poll_message.reactions:
-                if reaction.emoji in opt_dict.keys():
-                    reactors = await reaction.users().flatten()
-                    for reactor in reactors:
-                        if reactor.id not in voters:
-                            tally[reaction.emoji] += 1
-                            voters.append(reactor.id)
-            output = f"Results of the poll for '{embed.title}':\n" + '\n'.join(
-                [f'{opt_dict[key]}: {tally[key]}' for key in tally.keys()])
-            await ctx.send(output)
-            # TODO: End of mess
-
-        else:
-            await ctx.send("Correct syntax: `!poll <create/end>`")
 
     @commands.command(description="TOD0 list", usage="!todo")
     async def todo(self, ctx):
@@ -136,7 +60,7 @@ class Info(commands.Cog):
                 # Get a list of all commands in the specified cog
                 commands_list = self.bot.get_cog(cogs[lower_cogs.index(cog.lower())]).get_commands()
 
-                help_text = f'__{cog}:__\n' + '\\_'*32 + '\n'
+                help_text = f'__{cog}:__\n' + '\\_' * 32 + '\n'
                 for command in commands_list:
                     help_text += f'__{command.name}__\n'
                     help_text += f'Description: {command.description}\n'
@@ -151,41 +75,6 @@ class Info(commands.Cog):
 
                 await ctx.send(embed=embed)  # send it
 
-    @commands.command(usage='!timer <amount> <unit>', description='Sets timer to <amount> <unit>')
-    async def timer(self, ctx: commands.Context, time: str, units: str):
-        """
-        Simple timer
-
-        :param ctx: channel where the command was used
-        :param time: time in units
-        :param units: units of time used
-        """
-        time_units = {
-            'second': ['second', 'seconds', 'sec', 's'],
-            'minute': ['minute', 'minutes', 'min', 'm'],
-            'hour': ['hour', 'hour', 'hr', 'h']
-        }
-
-        try:
-            seconds = int(time)
-        except ValueError:
-            await ctx.send(embed=bd.error_embed('Value error', 'Invalid number was passed'))
-            return
-
-        if units.lower() in time_units['second']:
-            pass
-        elif units.lower() in time_units['minute']:
-            seconds *= 60
-        elif units.lower() in time_units['hour']:
-            seconds *= 3600
-        else:
-            await ctx.send(embed=bd.error_embed('Type error', 'Unknown unit of time'))
-            return
-
-        await ctx.send(f'Started timer for {time} {units}')
-        await aio.sleep(seconds)
-        await ctx.send(f'{ctx.author.mention} time is up!')
-
     @commands.command()
     async def ping(self, ctx: commands.Context):
         """
@@ -195,17 +84,6 @@ class Info(commands.Cog):
         :return: None
         """
         await ctx.send(f'Latency is {self.bot.latency*1000:.3f}ms')
-
-    # ----- repeat -----
-    @commands.command(description="speak beep boop", usage="!say [args]")
-    async def say(self, ctx: commands.Context, *, arg="Nothing"):
-        """
-        Friendly pudding's favourite command. Cannot be removed nor changed
-        :param ctx: channel where the command was used
-        :param arg: Message to say
-        :return: None
-        """
-        await ctx.send(str(arg))
 
 
 def setup(bot: commands.Bot):
